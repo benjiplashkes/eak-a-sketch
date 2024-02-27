562fc88270b2e877167371692bf6d30f7a309ce4
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-02-27 14:33:06 +0200

    Add basic function logic
    Grid rendering from button press
    Drawing with mouse over grid squares

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,46 @@
 const gridContainer = document.querySelector('#container')
 const button = document.querySelector('button#newGrid')
-button.addEventListener("click", handleClick)
 
-function handleClick(){}
+function handleClick(){
+    let gridSize = +prompt("Enter Grid Size - (Max 100)", 0)
+    if (gridSize > 100) {
+        gridSize = +prompt("Size too big!\nEnter Grid Size - (Max 100)", 0)
+    }
+    gridContainer.innerHTML = ""
+    const grid = createGrid(gridSize)
+    gridContainer.appendChild(grid)
+}
+function handleDraw(e){
+    e.currentTarget.classList.toggle('full')
+   
+}
+function createGrid(gridSize){
+    
+    function createRow(){
+        const row = document.createElement('div')
+        row.classList.add('row')
+        for(let i = 0; i < gridSize; i ++){
+            let square = document.createElement('div')
+            square.classList.add('square')
+            square.style.width = (960 / gridSize) + "px"
+            square.style.height = (960 / gridSize) + "px"
+            square.addEventListener('mouseenter', handleDraw)
+            row.appendChild(square)
+        }
+        return row
+    }
+    const grid = document.createElement('div')
+    grid.classList.add('grid-wrapper')
+    for(let i = 0; i < gridSize; i++){
+        let row = createRow()
+        grid.appendChild(row)
+    }
+    console.log(grid)
+    return grid
+}
+//draw default grid
+let newGrid = createGrid(16)
+gridContainer.appendChild(newGrid)
+
+//new grid button
+button.addEventListener("click", handleClick)
